0c887b749feb25a1137b939879b6886b889333c7
AhmedChabayta <chabays@gmail.com>
2023-08-29 18:38:43 +0300

    hid token in env

--- sanity/lib/client.ts
+++ sanity/lib/client.ts
@@ -7,6 +7,5 @@ export const client = createClient({
   dataset,
   projectId,
   useCdn,
-  token:
-    "skDqNw7ikpdoMJYvB8Y98dqViR9iQRBVibp1ZqfkHMcK5JyjEd5t0Sfe0iRa3oUZcSqMosM9FDdX8bKnGnByyUhacQvrggbWd4mlAClo1ispFQN7oIQyMnJBLxT4XieArGKjneGcGCtOMe8MHUs681Y0p66QV7OD4vnhg6KgdkU3szKYX5C4",
+  token,
 });
